fb5b478b1dbbdeea437ab57897c6933fcc3b294d
Lee Seung Ho <lsho@charmeng.com>
2022-05-16 17:03:05 +0900

    0516

--- dp/bag_12865.py
+++ dp/bag_12865.py
@@ -1,1 +1,20 @@
-N, M = map(int, input().split())
+N, K = map(int, input().split())
+li = [[0,0]]
+dp = [[0]*(K+1) for i in range(N+1)]
+for _ in range(N):
+    li.append(list(map(int, input().split())))
+
+li.sort(key=lambda x:x[0])
+
+for i in range(N+1):
+    for j in range(K+1):
+        weight = li[i][0]
+        value = li[i][1]
+
+        if j < weight:
+            dp[i][j] = dp[i-1][j]
+        else : 
+            dp[i][j] = max(dp[i-1][j],dp[i-1][j-weight]+value)
+
+print(dp[N][K])
+
