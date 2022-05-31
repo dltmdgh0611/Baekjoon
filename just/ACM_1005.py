b5e1a660fea4d53a43ecfaa79801dfcc18052781
Lee Seung Ho <lsho@charmeng.com>
2022-05-31 17:34:21 +0900

    0531

--- just/ACM_1005.py
+++ just/ACM_1005.py
@@ -1,29 +1,34 @@
+
 T = int(input())
+
+def xmax(rule, tech, time):
+    li = []
+    if len(rule[tech]) == 0:
+        return time[tech-1]
+    else :
+        for i in rule[tech]:
+            xmax(rule, i, time)
+            li.append(time[i-1])
+        time[tech-1] += max(li)
+        rule[tech] = []
+    return time[tech-1]
+            
+    
 
 for _ in range(T):
     N, K = map(int, input().split())
     buildtime = list(map(int, input().split()))
-    buildrule = []
+    buildrule = {}
     buildingInfo = []
     buildingInfo.append(buildtime[0])
+    for i in range(1,N+1):
+        buildrule[i] = []
     for i in range(K):
-        buildrule.append(list(map(int, input().split())))
+        buildli = list(map(int, input().split()))
+        buildrule[buildli[1]].append(buildli[0])
     W = int(input())
-
-    buildrule.sort()
     
-    for i in range(2,N+1):
-        techlist = []
-        totaltime = buildtime[i-1]
-        tech = []
-        for a in buildrule:
-            if a[1] == i:
-                tech.append(a[0])
-        for b in tech:
-            techlist.append(buildingInfo[b-1])
-        totaltime+=max(techlist)
-        buildingInfo.append(totaltime)
-    print(buildingInfo[W-1])
+    print(xmax(buildrule, W ,buildtime))
 
 
 
